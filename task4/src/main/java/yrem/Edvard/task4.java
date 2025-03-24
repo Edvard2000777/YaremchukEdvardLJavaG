89d3547b6dd7b6ecea325cfd4f87cb40603412b5
Edvard2000777 <0563166@gmail.com>
2025-03-24 13:27:33 +0400

    Tasks1234

--- task4/src/main/java/yrem/Edvard/task4.java
+++ task4/src/main/java/yrem/Edvard/task4.java
@@ -9,6 +9,11 @@ import java.util.Scanner;
 
 public class task4 {
     public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+
         if (args.length < 1) {
             System.out.println("Использование: java CommandLineArgsExample <путь_к_файлу_с_числами>");
             return;
